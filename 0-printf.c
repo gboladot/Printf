069c6c57e7a8a9d0cbef6e229b56d37d668d3ddf
CrystalXclusive <78354782+crystalxclusive@users.noreply.github.com>
2022-10-18 21:32:44 +0000

    Update 0-printf.c

--- 0-printf.c
+++ 0-printf.c
@@ -2,6 +2,11 @@
 
 void print_buffer(char buffer[], int *buff_ind);
 
+/**
+* _printf - a function that prints formouted output to stdout
+* @format: the string that contains any format specifier
+* Return: returns the number of printed characters
+*/
 int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
@@ -46,6 +51,11 @@ int _printf(const char *format, ...)
 	return (printed_chars);
 }
 
+/**
+*print_buffer - Prints the contents of the buffer if it exist
+* @buffer: Array of chars
+* @buff_ind: Index at which to add next char, represents the length.
+*/
 void print_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
